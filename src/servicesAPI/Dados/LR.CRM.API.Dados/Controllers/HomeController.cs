5178965e026d69f3366c8c46fb79366c79ba095b
Leonardo Machado <72328522+MRmachad@users.noreply.github.com>
2023-02-09 00:02:02 -0300

    fist commit

---
+++ src/servicesAPI/Dados/LR.CRM.API.Dados/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LR.CRM.API.Dados.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+       
+        public HomeController()
+        {
+
+        }
+
+        [HttpGet("/busca")]
+        public async Task<string> Busca()
+        {
+            var client = new HttpClient();
+
+            var res = await client.GetAsync("https://www.google.com.br/maps/search/escola/@-16.6652008,-49.2497189,15z");
+
+            return await res.Content.ReadAsStringAsync(); 
+        }
+    }
+}
